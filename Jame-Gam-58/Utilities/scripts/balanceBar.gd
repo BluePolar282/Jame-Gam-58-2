77b80095ba550f1792fe076aef9ff3d2607d504f
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-14 01:07:57 -0400

    now bar falls (hopefully)

--- Jame-Gam-58/Utilities/scripts/balanceBar.gd
+++ Jame-Gam-58/Utilities/scripts/balanceBar.gd
@@ -6,11 +6,24 @@ const GRAVITY = 2.0
 const MOUSE_PULL = 0.5   
 
 func _process(delta):
-	var mouse_pos = get_global_mouse_position()
-	
-	velocity += early_head.global_position.x * GRAVITY * delta
-	
-	var pull = (mouse_pos.x - early_head.global_position.x) * MOUSE_PULL * delta
-	velocity += pull
-	
-	early_head.global_position.x += velocity * delta
+	if early_head.global_position.x <= -300 or early_head.global_position.x >= 300 : 
+		print("pass point of no return")
+		
+		velocity += early_head.global_position.x * GRAVITY * delta
+		
+		var pull = (0 - early_head.global_position.x) * MOUSE_PULL * delta
+		velocity += pull
+		
+		early_head.global_position.x += velocity * delta
+		
+	else:
+		
+		var mouse_pos = get_global_mouse_position()
+		
+		velocity += early_head.global_position.x * GRAVITY * delta
+		
+		var pull = (mouse_pos.x - early_head.global_position.x) * MOUSE_PULL * delta
+		velocity += pull
+		
+		early_head.global_position.x += velocity * delta
+		print("mouse works")
