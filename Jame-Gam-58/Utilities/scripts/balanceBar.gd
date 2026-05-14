8127bcb717394367e4fadf734c4a3f6f4a726062
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-14 01:02:50 -0400

    Revert "Balance bar falls when moved too far"
    This reverts commit 85c089480afeca5e705e17e8a0600a38d31d9657.

--- Jame-Gam-58/Utilities/scripts/balanceBar.gd
+++ Jame-Gam-58/Utilities/scripts/balanceBar.gd
@@ -3,31 +3,14 @@ extends Sprite2D
 
 var velocity = 0.0
 const GRAVITY = 2.0      
-const MOUSE_PULL = 0.8   
-
-
-
+const MOUSE_PULL = 0.5   
 
 func _process(delta):
+	var mouse_pos = get_global_mouse_position()
 	
-	if early_head.global_position.x <= -300 or early_head.global_position.x >= 300 : 
-		print("pass point of no return")
-		
-		velocity += early_head.global_position.x * GRAVITY * delta
-		
-		var pull = (0 - early_head.global_position.x) * MOUSE_PULL * delta
-		velocity += pull
-		
-		early_head.global_position.x += velocity * delta
-		
-	else:
-		
-		var mouse_pos = get_global_mouse_position()
-		
-		velocity += early_head.global_position.x * GRAVITY * delta
-		
-		var pull = (mouse_pos.x - early_head.global_position.x) * MOUSE_PULL * delta
-		velocity += pull
-		
-		early_head.global_position.x += velocity * delta
-		print(123123)
+	velocity += early_head.global_position.x * GRAVITY * delta
+	
+	var pull = (mouse_pos.x - early_head.global_position.x) * MOUSE_PULL * delta
+	velocity += pull
+	
+	early_head.global_position.x += velocity * delta
