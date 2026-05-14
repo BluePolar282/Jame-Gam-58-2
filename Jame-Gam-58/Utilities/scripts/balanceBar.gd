0ff5fc7ae977ad6613ed5abd5bea4c7264d6ec75
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-13 23:48:02 -0400

    mouse now has gravity

--- Jame-Gam-58/Utilities/scripts/balanceBar.gd
+++ Jame-Gam-58/Utilities/scripts/balanceBar.gd
@@ -1,15 +1,16 @@
 extends Sprite2D
-
 @onready var early_head: Sprite2D = $earlyHead
 
-const MIN_X = -284
-const MAX_X = 282
-
+var velocity = 0.0
+const GRAVITY = 2.0      
+const MOUSE_PULL = 0.5   
 
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	
-	var raw_dir = mouse_pos - early_head.global_position
-	early_head.global_position.x += raw_dir.x * delta
-	early_head.global_position.x = clamp(early_head.global_position.x, MIN_X, MAX_X)
-	print(early_head.global_position.x)
+	velocity += early_head.global_position.x * GRAVITY * delta
+	
+	var pull = (mouse_pos.x - early_head.global_position.x) * MOUSE_PULL * delta
+	velocity += pull
+	
+	early_head.global_position.x += velocity * delta
