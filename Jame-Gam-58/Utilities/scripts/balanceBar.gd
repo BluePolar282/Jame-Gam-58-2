9e9893f26ebfb88b871e920cafd7249d24805d58
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-14 03:01:03 -0400

    Bar is now velocity based
    Was location based, now velocity based

--- Jame-Gam-58/Utilities/scripts/balanceBar.gd
+++ Jame-Gam-58/Utilities/scripts/balanceBar.gd
@@ -2,28 +2,33 @@ extends Sprite2D
 @onready var early_head: Sprite2D = $earlyHead
 
 var velocity = 0.0
-const GRAVITY = 2.0      
-const MOUSE_PULL = 0.5   
+var last_mouse_pos = Vector2.ZERO
+const GRAVITY = 3.0
+const MOUSE_FORCE = 0.5
+
+func _ready():
+	last_mouse_pos = get_local_mouse_position()
 
 func _process(delta):
 	if early_head.global_position.x <= -300 or early_head.global_position.x >= 300 : 
-		print("pass point of no return")
-		
-		velocity += early_head.global_position.x * GRAVITY * delta
-		
-		var pull = (0 - early_head.global_position.x) * MOUSE_PULL * delta
-		velocity += pull
-		
-		early_head.global_position.x += velocity * delta
-		
-	else:
-		
 		var mouse_pos = get_global_mouse_position()
 		
+		var mouse_delta = mouse_pos.x - last_mouse_pos.x
+		last_mouse_pos = mouse_pos
+		
 		velocity += early_head.global_position.x * GRAVITY * delta
 		
-		var pull = (mouse_pos.x - early_head.global_position.x) * MOUSE_PULL * delta
-		velocity += pull
+		velocity += mouse_delta * MOUSE_FORCE
 		
 		early_head.global_position.x += velocity * delta
-		print("mouse works")
+	else:
+		var mouse_pos = get_global_mouse_position()
+		
+		var mouse_delta = mouse_pos.x - last_mouse_pos.x
+		last_mouse_pos = mouse_pos
+		
+		velocity += early_head.global_position.x * GRAVITY * delta
+		
+		velocity += mouse_delta * MOUSE_FORCE
+		
+		early_head.global_position.x += velocity * delta
