948a3ce6315570dbccce70485bb1373f04d4d333
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-14 23:01:05 -0400

    Balance bar in game (old)

--- Jame-Gam-58/Utilities/scripts/balanceBar.gd
+++ Jame-Gam-58/Utilities/scripts/balanceBar.gd
@@ -7,28 +7,17 @@ const GRAVITY = 3.0
 const MOUSE_FORCE = 2.5
 
 func _ready():
-	last_mouse_pos = get_local_mouse_position()
+	last_mouse_pos = get_global_mouse_position()  # Fixed, was get_local
 
 func _process(delta):
-	if early_head.global_position.x <= -300 or early_head.global_position.x >= 300 : 
-		var mouse_pos = get_global_mouse_position()
-		
-		var mouse_delta = mouse_pos.x - last_mouse_pos.x
-		last_mouse_pos = mouse_pos
-		
-		velocity += early_head.global_position.x * GRAVITY * delta
-		
-		velocity += mouse_delta * MOUSE_FORCE
-		
-		early_head.global_position.x += velocity * delta
-	else:
-		var mouse_pos = get_global_mouse_position()
-		
-		var mouse_delta = mouse_pos.x - last_mouse_pos.x
-		last_mouse_pos = mouse_pos
-		
-		velocity += early_head.global_position.x * GRAVITY * delta
-		
-		velocity += mouse_delta * MOUSE_FORCE
-		
-		early_head.global_position.x += velocity * delta
+	var mouse_pos = get_global_mouse_position()
+	var mouse_delta = mouse_pos.x - last_mouse_pos.x
+	last_mouse_pos = mouse_pos
+	
+	velocity += (early_head.global_position.x - global_position.x) * GRAVITY * delta
+	velocity += mouse_delta * MOUSE_FORCE
+	early_head.global_position.x += velocity * delta
+	
+	
+	if early_head.global_position.x <= -300 or early_head.global_position.x >= 300:
+		pass 
