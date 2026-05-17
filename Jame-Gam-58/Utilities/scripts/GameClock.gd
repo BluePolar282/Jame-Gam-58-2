98001de3981989c4554729334772d32ac3c50b9c
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-17 03:54:50 -0400

    ProgressBar now inplace

--- Jame-Gam-58/Utilities/scripts/GameClock.gd
+++ Jame-Gam-58/Utilities/scripts/GameClock.gd
@@ -1,10 +1,7 @@
 extends TextureProgressBar
 
-var time_limit = 15
-var elapsed = 0.0
-
 func _process(delta):
-	
-	
-	elapsed += delta
-	value = (elapsed / time_limit) * 100.0 
+	var stages_done = Globals.stage - 1  # completed stages
+	var stage_progress = 1.0 - (Globals.time_left / 45.0)  # progress within current stage
+	var total_progress = (stages_done + stage_progress) / 5.0
+	value = total_progress * 100.0
