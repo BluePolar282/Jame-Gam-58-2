b1c7a2e98d181e20fce12a798591ca103f18678c
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-17 07:05:26 -0400

    BIG update

--- Jame-Gam-58/Utilities/scenes/main_scene.gd
+++ Jame-Gam-58/Utilities/scenes/main_scene.gd
@@ -2,3 +2,11 @@ extends Node2D
 
 func _ready() -> void:
 	Globals.in_main_scene = true
+	if Globals.has_won == true:
+		print("night")
+		$sunSet.visible = false
+		$nightTime.visible = true
+	elif Globals.has_won == false:
+		print("day")
+		$sunSet.visible = true
+		$nightTime.visible = false
