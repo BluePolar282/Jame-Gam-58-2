e3cc5b992954f4fd50f7bdf22078389223788f60
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-16 22:40:21 -0400

    Sfx stuff maybe

--- Jame-Gam-58/UI/scripts/button_effects_module.gd
+++ Jame-Gam-58/UI/scripts/button_effects_module.gd
@@ -1,29 +1,31 @@
 extends Node
 class_name ButtonEffectsModule
-
 @export var ease_type: Tween.EaseType
 @export var trans_type: Tween.TransitionType
 @export var anim_duration := 0.07
 @export var scale_amount := Vector2(1.1, 1.1)
 @export var rotation_amount := 3.0
-
-@onready var button : Button = get_parent()
-
+@onready var button: Button = get_parent()
+@onready var hoverOn: AudioStreamPlayer2D = $hoverOn
+@onready var hoverOff: AudioStreamPlayer2D = $hoverOff
 var tween: Tween
+
 
 func _ready() -> void:
 	button.mouse_entered.connect(_on_mouse_hovered.bind(true))
 	button.mouse_exited.connect(_on_mouse_hovered.bind(false))
-	
+
 func _on_mouse_hovered(hovered: bool) -> void:
 	reset_tween()
 	tween.tween_property(button, "scale",
 		scale_amount if hovered else Vector2.ONE, anim_duration)
 	tween.tween_property(button, "rotation_degrees",
 		rotation_amount * -0.5 if hovered else 0.0, anim_duration)
-
-
-
+	
+	if hovered:
+		hoverOn.play()
+	else:
+		hoverOff.play()
 
 func reset_tween() -> void:
 	if tween:
