f3a6821d4cc0886781064c77a827e395d10e3e2d
Purple-Nurple44 <noah08isaac09@gmail.com>
2026-05-17 10:11:30 -0400

    bulletthing

--- Jame-Gam-58/Utilities/scripts/bullet_spawner.gd
+++ Jame-Gam-58/Utilities/scripts/bullet_spawner.gd
@@ -35,7 +35,7 @@ func spawn_bullets() -> void:
 
 	if Globals.on_cooldown:
 		return
-	if spawner_group in ["Top Left Spawner", "Top Right Spawner"] and Globals.stage < 2:
+	if spawner_group in ["Top Left Spawner", "Top Right Spawner"] and Globals.stage <= 1:
 		return
 
 	var fire_chance := set_bullet_chance()
@@ -45,31 +45,15 @@ func spawn_bullets() -> void:
 	var spawned_bullet = bullet.instantiate()
 	var spawned_bouncy_bullet = bouncy_bullet.instantiate()
 	
-	if Globals.stage <= 2:
+	if rand > 0.1:
 		add_child(spawned_bullet)
 		spawned_bullet.add_to_group(spawner_group)
-	
-	if Globals.stage == 3:
-		if rand > 0.2:
-			add_child(spawned_bullet)
-			spawned_bullet.add_to_group(spawner_group)
-		if rand < 0.1:
-			add_child(spawned_bouncy_bullet)
-			spawned_bouncy_bullet.add_to_group(spawner_group)
-			bouncy_in_scene = true
-			await get_tree().create_timer(1.5).timeout
-			bouncy_in_scene = false
-	elif Globals.stage > 3:
-		if rand > 0.4:
-			add_child(spawned_bullet)
-			spawned_bullet.add_to_group(spawner_group)
-		if rand < 0.2:
-			add_child(spawned_bouncy_bullet)
-			spawned_bouncy_bullet.add_to_group(spawner_group)
-			bouncy_in_scene = true
-			await get_tree().create_timer(1.5).timeout
-			bouncy_in_scene = false
-
+	if rand < 0.1 and Globals.stage > 2:
+		add_child(spawned_bouncy_bullet)
+		spawned_bouncy_bullet.add_to_group(spawner_group)
+		bouncy_in_scene = true
+		await get_tree().create_timer(1.5).timeout
+		bouncy_in_scene = false
 	
 	Globals.current_dir = spawner_dir
 
@@ -90,10 +74,10 @@ func set_difficulty() -> void:
 	if stage == 1 and !bouncy_in_scene:
 		$Timer.wait_time = randf_range(1, 2)
 	if stage == 2 and !bouncy_in_scene:
-		$Timer.wait_time = randf_range(0.7, 1)
+		$Timer.wait_time = randf_range(0.5, 1)
 	if stage == 3 and !bouncy_in_scene:
-		$Timer.wait_time = randf_range(0.4, 0.9)
+		$Timer.wait_time = randf_range(0.25, 0.5)
 	if stage > 4 and !bouncy_in_scene:
-		$Timer.wait_time = randf_range(0.2, 0.4)
+		$Timer.wait_time = randf_range(0.1, 0.3)
 	elif stage > 0 and bouncy_in_scene:
 		$Timer.wait_time = randf_range(1, 2)
